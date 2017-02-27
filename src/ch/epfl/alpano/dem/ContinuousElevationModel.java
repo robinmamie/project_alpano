d0cfbca534b058c6a28a07a140b084d888d2bd32
Robin G.F. Mamie <robin.mamie@gmail.com>
2017-02-27 23:45:43 +0100

    fixed slope

--- src/ch/epfl/alpano/dem/ContinuousElevationModel.java
+++ src/ch/epfl/alpano/dem/ContinuousElevationModel.java
@@ -16,14 +16,16 @@ import ch.epfl.alpano.Math2;
  * @author Maxence Jouve (269716)
  */
 public final class ContinuousElevationModel {
-    
-    
+
+
     /**
      * MNT discret utilisé.
      */
     private DiscreteElevationModel dem;
 
-    
+    private static final double d = Distance.toMeters(1 / SAMPLES_PER_RADIAN);
+
+
     /**
      * Construit un MNT continu à partir d'un MNT discret.
      * 
@@ -36,46 +38,55 @@ public final class ContinuousElevationModel {
     public ContinuousElevationModel(DiscreteElevationModel dem) {
         this.dem = requireNonNull(dem);
     }
-    
-    
+
+    private double elevationAtIndex(int x, int y) {
+        return dem.elevationSample(x, y);
+    }
+
+    private double slopeAtIndex(int x, int y) {
+        double a = elevationAtIndex(x  , y  );
+        double b = elevationAtIndex(x+1, y  );
+        double c = elevationAtIndex(x  , y+1);
+        return Math.acos(d / Math.sqrt( Math2.sq(b-a) + Math2.sq(c-a) + d*d ));
+    }
+
+
     private double[] parametersForBilerp(GeoPoint p, boolean slope) {
         double[] t = new double[6];
-        
+
         double aIndex = sampleIndex(p.longitude());
         double bIndex = sampleIndex(p.latitude());
-        
+
         int a = (int)Math.floor(aIndex);
         int b = (int)Math.floor(bIndex);
-        
+
         try {
-        t[0] = dem.elevationSample(a  , b  );
-        t[1] = dem.elevationSample(a+1, b  );
-        t[3] = dem.elevationSample(a  , b+1);
-        t[2] = dem.elevationSample(a+1, b+1);
+            for(int i = 0; i < 4; ++i) {
+                int addA = (i%2 == 1) ? 1 : 0;
+                int addB = (i > 1) ? 1 : 0;
+                if(slope)
+                    t[i] = slopeAtIndex(a + addA, b + addB);
+                else
+                    t[i] = elevationAtIndex(a + addA, b + addB);
+                
+            }
         } catch(IllegalArgumentException e) {
             return new double[6];
         }
-        
-        if(slope) {
-            double[] s = new double[4];
-            final double d = Distance.toMeters(1 / SAMPLES_PER_RADIAN);
-            for(int i = 0; i < 4; ++i)
-                s[i] = Math.acos(d / Math.sqrt( Math2.sq(t[(i+1)%4] - t[i]) + Math2.sq(t[(i+2)%4] - t[i]) + d*d ));
-            for(int i = 0; i < 4; ++i)
-                t[i] = s[i];
-        }
-        
-        double temp = t[2];
-        t[2] = t[3];
-        t[3] = temp;
-        
+
         t[4]   = aIndex - a;
         t[5]   = bIndex - b;
-        
+
         return t;
     }
     
-    
+    private double bilerpChoice(GeoPoint p, boolean slope) {
+        double[] a = parametersForBilerp(p, slope);
+
+        return Math2.bilerp(a[0], a[1], a[2], a[3], a[4], a[5]);
+    }
+
+
     /**
      * Retourne l'altitude au point donné, en mètres. Elle est obtenue par
      * interpolation bilinéaire du MNT discret donné au constructeur.
@@ -86,12 +97,10 @@ public final class ContinuousElevationModel {
      * @return l'altitude au point <code>p</code>
      */
     public double elevationAt(GeoPoint p) {
-        double[] a = parametersForBilerp(p, false);
-        
-        return Math2.bilerp(a[0], a[1], a[2], a[3], a[4], a[5]);
+        return bilerpChoice(p, false);
     }
-    
-    
+
+
     /**
      * Retourne la pente du point donné, en radians. Elle est obtenue par
      * interpolation bilinéaire du MNT discret donné au constructeur.
@@ -102,9 +111,7 @@ public final class ContinuousElevationModel {
      * @return la pente au point <code>p</code>
      */
     public double slopeAt(GeoPoint p) {
-        double[] a = parametersForBilerp(p, true);            
-        
-        return Math2.bilerp(a[0], a[1], a[2], a[3], a[4], a[5]);
+        return bilerpChoice(p, true);
     }
 
 }
