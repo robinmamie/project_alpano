6eda91404ebd6f0bb2204126056b77cd91a8a107
Robin G.F. Mamie <robin.mamie@gmail.com>
2017-03-10 12:44:06 +0100

    updated style of checking

--- src/ch/epfl/alpano/dem/HgtDiscreteElevationModel.java
+++ src/ch/epfl/alpano/dem/HgtDiscreteElevationModel.java
@@ -13,33 +13,17 @@ import ch.epfl.alpano.Interval2D;
 
 public final class HgtDiscreteElevationModel implements DiscreteElevationModel {
 
+    private static final int SIDE = SAMPLES_PER_DEGREE + 1;
     private final FileInputStream stream;
     private ShortBuffer source;
-    private static final int SIDE = SAMPLES_PER_DEGREE + 1;
-    private int lonIndex;
-    private int latIndex;
+
+    private final int lonIndex;
+    private final int latIndex;
     private final Interval2D ext;
 
     public HgtDiscreteElevationModel(File file) {
-        final long l = file.length();
-        final int limit =  2 * SIDE * SIDE;
-        checkArgument(valid(file), "file name invalid");
-        checkArgument(l == limit,  "file does not comply to byte restrictions");
-        
-        try {
-            stream = new FileInputStream(file);
-            source = stream.getChannel()
-              .map(MapMode.READ_ONLY, 0, l)
-              .asShortBuffer();
-          } catch(IOException e) {
-              throw new IllegalArgumentException("file invalid");
-          }
-        
-        this.ext = extent();
-    }
-
-    private boolean valid(File file) {
-        String n = file.getName();
+        String n   = file.getName();
+        String fni = "file name invalid: ";
 
         char ns = n.charAt(0);
         char ew = n.charAt(3);
@@ -49,22 +33,45 @@ public final class HgtDiscreteElevationModel implements DiscreteElevationModel {
             lat = Integer.parseInt(n.substring(1,3));
             lon = Integer.parseInt(n.substring(4,7));
         } catch(NumberFormatException e) {
-            return false;
+            throw new IllegalArgumentException(fni + "does not contain numbers at the right places");
         }
-        
+
         lonIndex = (ew == 'E' ? 1 : -1) * lon * SAMPLES_PER_DEGREE;
         latIndex = (ns == 'N' ? 1 : -1) * lat * SAMPLES_PER_DEGREE;
-        
-        boolean valid = !(n.length() != 11
-                || ns != 'N' &&  ns != 'S'
-                || ns == 'N' &&  90 <= lat
-                || ns == 'S' &&  90 <  lat
-                || ew != 'E' &&  ew != 'W'
-                || ew == 'E' && 180 <= lon
-                || ew == 'W' && 180 <  lon
-                || !n.substring(7, 11).equals(".hgt"));
 
-        return valid;
+        checkArgument(n.length() == 11
+                , fni + "too short or too long");
+        checkArgument(ns == 'N' ||  ns == 'S'
+                , fni + "isn't defined for North or South");
+        checkArgument(ns != 'N' ||  lat < 90
+                , fni + "northern latitude impossible");
+        checkArgument(lat <= 90
+                , fni + "latitude impossible");
+        checkArgument(ew == 'E' ||  ew == 'W'
+                , fni + "isn't defined for East or West");
+        checkArgument(ew != 'E' || lon < 180
+                , fni + "eastern longitude impossible");
+        checkArgument(lon <= 180
+                , fni + "longitude impossible");
+        checkArgument(n.substring(7, 11).equals(".hgt")
+                , fni + "doesn't end with correct extension");
+
+
+        final long l = file.length();
+
+        checkArgument(l == 2 * SIDE * SIDE
+                ,  "file invalid: does not comply to byte restrictions");
+
+        try {
+            stream = new FileInputStream(file);
+            source = stream.getChannel()
+                    .map(MapMode.READ_ONLY, 0, l)
+                    .asShortBuffer();
+        } catch(IOException e) {
+            throw new IllegalArgumentException("file invalid");
+        }
+
+        this.ext = extent();
     }
 
     @Override
@@ -83,10 +90,10 @@ public final class HgtDiscreteElevationModel implements DiscreteElevationModel {
     @Override
     public double elevationSample(int x, int y) {
         checkArgument(ext.contains(x, y), "the HgtDEM does not contain the given index");
-        
+
         int ySize = latIndex + SIDE - 1 - y;
         int xSize = x - lonIndex;
-        
+
         return source.get(ySize * SIDE + xSize);
     }
 
