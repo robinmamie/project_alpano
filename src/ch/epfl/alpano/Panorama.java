59ddfb3a438f61a877725bceb9e157477335ba58
Robin G.F. Mamie <robin.mamie@gmail.com>
2017-03-20 20:25:01 +0100

    javadoc for part 6 complete

--- src/ch/epfl/alpano/Panorama.java
+++ src/ch/epfl/alpano/Panorama.java
@@ -4,6 +4,18 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.Arrays;
 
+/**
+ * Classe contenant toutes les informations utiles à la représentation graphique
+ * ou textuelle d'un panorama. Elle donne plusieurs informations à propos de
+ * chaque point le composant: sa distance par rapport à l'observateur, sa
+ * position, son élévation et sa pente.
+ * 
+ * <p>
+ * Classe immuable ne pouvant être construite qu'à l'aide de son Builder.
+ *
+ * @author Robin Mamie (257234)
+ * @author Maxence Jouve (269716)
+ */
 public final class Panorama {
 
     private final PanoramaParameters parameters;
@@ -20,6 +32,11 @@ public final class Panorama {
         this.slope = slope;
     }
 
+    /**
+     * Retourne les paramètres du Panorama.
+     * 
+     * @return Les paramètres du Panorama.
+     */
     public PanoramaParameters parameters() {
         return parameters;
     }
@@ -29,42 +46,150 @@ public final class Panorama {
             throw new IndexOutOfBoundsException(m);
     }
 
-    public float distanceAt(int x, int y) throws IndexOutOfBoundsException {
+    /**
+     * Retourne la distance du point <i>(x,y)</i> du Panorama par rapport à
+     * l'observateur ou lève l'exception IndexOutOfBoundsException si le point
+     * n'est pas défini dans le Panorama.
+     * 
+     * @param x
+     *            L'index horizontal.
+     * @param y
+     *            L'index vertical.
+     * 
+     * @return La distance du point <i>(x,y)</i> du Panorama par rapport à
+     *         l'observateur.
+     * 
+     * @throws IndexOutOfBoundsException
+     *             si le point n'est pas défini dans le Panorama.
+     */
+    public float distanceAt(int x, int y) {
         checkIndex(x, y, "Index not valid for this distance.");
         return distance[parameters().linearSampleIndex(x, y)];
     }
 
+    /**
+     * Retourne la distance du point <i>(x,y)</i> du Panorama par rapport à
+     * l'observateur si le point est défini dans le Panorama. Sinon, retourne la
+     * valeur par défaut passée en arguement.
+     * 
+     * @param x
+     *            L'index horizontal.
+     * @param y
+     *            L'index vertical.
+     * @param d
+     *            Valeur par défaut.
+     * 
+     * @return La distance du point <i>(x,y)</i> du Panorama par rapport à
+     *         l'observateur ou la valeur par défaut <i>d</i> passée en argument
+     *         si le point n'est pas défini dans le Panorama.
+     */
     public float distanceAt(int x, int y, float d) {
         if (!parameters.isValidSampleIndex(x, y))
             return d;
         return distanceAt(x, y);
     }
 
-    public float longitudeAt(int x, int y) throws IndexOutOfBoundsException {
+    /**
+     * Retourne la longitude du point <i>(x,y)</i> du Panorama ou lève
+     * l'exception IndexOutOfBoundsException si le point n'est pas défini dans
+     * le Panorama.
+     * 
+     * @param x
+     *            L'index horizontal.
+     * @param y
+     *            L'index vertical.
+     * 
+     * @return La longitude du point <i>(x,y)</i> du Panorama.
+     * 
+     * @throws IndexOutOfBoundsException
+     *             si le point n'est pas défini dans le Panorama.
+     */
+    public float longitudeAt(int x, int y) {
         checkIndex(x, y, "Index not valid for this longitude.");
         return longitude[parameters().linearSampleIndex(x, y)];
     }
 
-    public float latitudeAt(int x, int y) throws IndexOutOfBoundsException {
+    /**
+     * Retourne la latitude du point <i>(x,y)</i> du Panorama ou lève
+     * l'exception IndexOutOfBoundsException si le point n'est pas défini dans
+     * le Panorama.
+     * 
+     * @param x
+     *            L'index horizontal.
+     * @param y
+     *            L'index vertical.
+     * 
+     * @return La latitude du point <i>(x,y)</i> du Panorama.
+     * 
+     * @throws IndexOutOfBoundsException
+     *             si le point n'est pas défini dans le Panorama.
+     */
+    public float latitudeAt(int x, int y) {
         checkIndex(x, y, "Index not valid for this latitude.");
         return latitude[parameters().linearSampleIndex(x, y)];
     }
 
-    public float elevationAt(int x, int y) throws IndexOutOfBoundsException {
+    /**
+     * Retourne l'élévation du point <i>(x,y)</i> du Panorama ou lève
+     * l'exception IndexOutOfBoundsException si le point n'est pas défini dans
+     * le Panorama.
+     * 
+     * @param x
+     *            L'index horizontal.
+     * @param y
+     *            L'index vertical.
+     * 
+     * @return L'élévation du point <i>(x,y)</i> du Panorama.
+     * 
+     * @throws IndexOutOfBoundsException
+     *             si le point n'est pas défini dans le Panorama.
+     */
+    public float elevationAt(int x, int y) {
         checkIndex(x, y, "Index not valid for this altitude.");
         return elevation[parameters().linearSampleIndex(x, y)];
     }
 
-    public float slopeAt(int x, int y) throws IndexOutOfBoundsException {
+    /**
+     * Retourne la pente du point <i>(x,y)</i> du Panorama ou lève l'exception
+     * IndexOutOfBoundsException si le point n'est pas défini dans le Panorama.
+     * 
+     * @param x
+     *            L'index horizontal.
+     * @param y
+     *            L'index vertical.
+     * 
+     * @return La pente du point <i>(x,y)</i> du Panorama.
+     * 
+     * @throws IndexOutOfBoundsException
+     *             si le point n'est pas défini dans le Panorama.
+     */
+    public float slopeAt(int x, int y) {
         checkIndex(x, y, "Index not valid for this slope.");
         return slope[parameters().linearSampleIndex(x, y)];
     }
 
+    /**
+     * Classe utilitaire permettant de construire un panorama qui lui sera
+     * immuable.
+     *
+     * @author Robin Mamie (257234)
+     * @author Maxence Jouve (269716)
+     */
     public static final class Builder {
         private final PanoramaParameters parameters;
         private float[] distance, longitude, latitude, elevation, slope;
         private boolean built;
 
+        /**
+         * Constructeur du Builder de Panorama. Demande les paramètres du
+         * Panorama en argument.
+         * 
+         * @param parameters
+         *            Les paramètres du Panorama.
+         * 
+         * @throws NullPointerException
+         *             si le PanoramaParameters passé en argument est null.
+         */
         public Builder(PanoramaParameters parameters) {
             this.parameters = requireNonNull(parameters);
             final int size = parameters.width() * parameters.height();
@@ -86,46 +211,130 @@ public final class Panorama {
                         "Panorama Builder already built.");
         }
 
-        public Builder setDistanceAt(int x, int y, float distance)
-                throws IllegalStateException {
+        /**
+         * Redéfinit la distance du point <i>(x,y)</i> du Panorama par rapport à
+         * l'obervateur.
+         * 
+         * @param x
+         *            L'index horizontal.
+         * @param y
+         *            L'index vertical.
+         * @param distance
+         *            La distance qui va redéfinir le point.
+         * 
+         * @return L'instance actualisée du Builder.
+         * 
+         * @throws IllegalStateException
+         *             si un Panorama a déjà été construit à l'aide de ce
+         *             Builder.
+         */
+        public Builder setDistanceAt(int x, int y, float distance) {
             checkIfBuilt();
             this.distance[parameters.linearSampleIndex(x, y)] = distance;
             return this;
         }
 
-        public Builder setLongitudeAt(int x, int y, float longitude)
-                throws IllegalStateException {
+        /**
+         * Redéfinit la longitude du point <i>(x,y)</i> du Panorama.
+         * 
+         * @param x
+         *            L'index horizontal.
+         * @param y
+         *            L'index vertical.
+         * @param longitude
+         *            La longitude qui va redéfinir le point.
+         * 
+         * @return L'instance actualisée du Builder.
+         * 
+         * @throws IllegalStateException
+         *             si un Panorama a déjà été construit à l'aide de ce
+         *             Builder.
+         */
+        public Builder setLongitudeAt(int x, int y, float longitude) {
             checkIfBuilt();
             this.longitude[parameters.linearSampleIndex(x, y)] = longitude;
             return this;
 
         }
 
-        public Builder setLatitudeAt(int x, int y, float latitude)
-                throws IllegalStateException {
+        /**
+         * Redéfinit la latitude du point <i>(x,y)</i> du Panorama.
+         * 
+         * @param x
+         *            L'index horizontal.
+         * @param y
+         *            L'index vertical.
+         * @param longitude
+         *            La latitude qui va redéfinir le point.
+         * 
+         * @return L'instance actualisée du Builder.
+         * 
+         * @throws IllegalStateException
+         *             si un Panorama a déjà été construit à l'aide de ce
+         *             Builder.
+         */
+        public Builder setLatitudeAt(int x, int y, float latitude) {
             checkIfBuilt();
             this.latitude[parameters.linearSampleIndex(x, y)] = latitude;
             return this;
 
         }
 
-        public Builder setElevationAt(int x, int y, float elevation)
-                throws IllegalStateException {
+        /**
+         * Redéfinit l'élévation du point <i>(x,y)</i> du Panorama.
+         * 
+         * @param x
+         *            L'index horizontal.
+         * @param y
+         *            L'index vertical.
+         * @param longitude
+         *            L'élévation qui va redéfinir le point.
+         * 
+         * @return L'instance actualisée du Builder.
+         * 
+         * @throws IllegalStateException
+         *             si un Panorama a déjà été construit à l'aide de ce
+         *             Builder.
+         */
+        public Builder setElevationAt(int x, int y, float elevation) {
             checkIfBuilt();
             this.elevation[parameters.linearSampleIndex(x, y)] = elevation;
             return this;
 
         }
 
-        public Builder setSlopeAt(int x, int y, float slope)
-                throws IllegalStateException {
+        /**
+         * Redéfinit la pente du point <i>(x,y)</i> du Panorama.
+         * 
+         * @param x
+         *            L'index horizontal.
+         * @param y
+         *            L'index vertical.
+         * @param longitude
+         *            La pente qui va redéfinir le point.
+         * 
+         * @return L'instance actualisée du Builder.
+         * 
+         * @throws IllegalStateException
+         *             si un Panorama a déjà été construit à l'aide de ce
+         *             Builder.
+         */
+        public Builder setSlopeAt(int x, int y, float slope) {
             checkIfBuilt();
             this.slope[parameters.linearSampleIndex(x, y)] = slope;
             return this;
         }
 
-        public Panorama build()
-                throws IllegalStateException {
+        /**
+         * Construit un Panorama comportant les éléments courants du Builder.
+         * 
+         * @return Le Panorama comportant les éléments courants du Builder.
+         * 
+         * @throws IllegalStateException
+         *             si un Panorama a déjà été construit à l'aide de ce
+         *             Builder.
+         */
+        public Panorama build() {
             checkIfBuilt();
             this.built = true;
             return new Panorama(parameters, distance, longitude, latitude,
--- src/ch/epfl/alpano/PanoramaComputer.java
+++ src/ch/epfl/alpano/PanoramaComputer.java
@@ -1,9 +1,10 @@
 package ch.epfl.alpano;
 
-import static ch.epfl.alpano.Math2.sq;
 import static ch.epfl.alpano.Math2.firstIntervalContainingRoot;
 import static ch.epfl.alpano.Math2.improveRoot;
-
+import static ch.epfl.alpano.Math2.sq;
+import static ch.epfl.alpano.Distance.EARTH_RADIUS;
+import static java.lang.Math.cos;
 import static java.lang.Math.tan;
 import static java.util.Objects.requireNonNull;
 
@@ -12,19 +13,44 @@ import java.util.function.DoubleUnaryOperator;
 import ch.epfl.alpano.dem.ContinuousElevationModel;
 import ch.epfl.alpano.dem.ElevationProfile;
 
+/**
+ * Classe permettant de calculer un Panorama à l'aide d'un MNT continu. Classe
+ * immuable.
+ *
+ * @author Robin Mamie (257234)
+ * @author Maxence Jouve (269716)
+ */
 public final class PanoramaComputer {
 
-    private static final double factor = (1.0 - 0.13)
-            / (2 * Distance.EARTH_RADIUS);
+    private static final double k = 0.13;
+    private static final double factor = (1.0 - k) / (2 * EARTH_RADIUS);
     private static final double limit = 64.0;
     private static final double epsilon = 4.0;
 
     private final ContinuousElevationModel dem;
 
+    /**
+     * Constructeur de la classe PanoramaComputer prenant un MNT continu en
+     * argument.
+     * 
+     * @param dem
+     *            Le MNT continu qui permettra de construire le Panorama.
+     * 
+     * @throws NullPointerException
+     *             si le MNT passé en argument est null.
+     */
     public PanoramaComputer(ContinuousElevationModel dem) {
         this.dem = requireNonNull(dem);
     }
 
+    /**
+     * Calcule le Panorama à l'aide des paramètres passés en argument.
+     * 
+     * @param parameters
+     *            Les paramètres qui définissent le Panorama à construire.
+     * 
+     * @return Le Panorama à l'aide des paramètres passés en argument.
+     */
     public Panorama computePanorama(PanoramaParameters parameters) {
         Panorama.Builder pb = new Panorama.Builder(parameters);
         double angle;
@@ -32,19 +58,18 @@ public final class PanoramaComputer {
             ElevationProfile profile = new ElevationProfile(dem,
                     parameters.observerPosition(), parameters.azimuthForX(x),
                     parameters.maxDistance());
-            double[] val = new double[parameters.height()+1];
+            double[] val = new double[parameters.height() + 1];
             for (int y = parameters.height() - 1; y >= 0; --y) {
                 angle = parameters.altitudeForY(y);
                 DoubleUnaryOperator f = rayToGroundDistance(profile,
                         parameters.observerElevation(), angle);
-                val[y] = firstIntervalContainingRoot(f,
-                        val[y+1],
+                val[y] = firstIntervalContainingRoot(f, val[y + 1],
                         parameters.maxDistance(), limit);
                 if (val[y] == Double.POSITIVE_INFINITY)
                     break;
                 val[y] = improveRoot(f, val[y], val[y] + limit, epsilon);
                 GeoPoint point = profile.positionAt(val[y]);
-                pb.setDistanceAt(x, y, (float) (val[y] / Math.cos(angle)))
+                pb.setDistanceAt(x, y, (float) (val[y] / cos(angle)))
                         .setLongitudeAt(x, y, (float) point.longitude())
                         .setLatitudeAt(x, y, (float) point.latitude())
                         .setElevationAt(x, y,
@@ -55,6 +80,21 @@ public final class PanoramaComputer {
         return pb.build();
     }
 
+    /**
+     * Définit la fonction donnant la distance des rayons-vecteurs lancés depuis
+     * l'observateur par rapport au sol.
+     * 
+     * @param profile
+     *            Le profil calculé au préalable. Permet de connaître l'altitude
+     *            du sol en tout point sous le rayon vecteur.
+     * @param ray0
+     *            L'élévation de l'obervateur d'où partent les rayons-vecteurs.
+     * @param raySlope
+     *            La pente du rayon-vecteur.
+     * 
+     * @return Un DoubleUnaryOperator donnant la distance des rayons-vecteurs
+     *         lancés depuis l'observateur par rapport au sol.
+     */
     public static DoubleUnaryOperator rayToGroundDistance(
             ElevationProfile profile, double ray0, double raySlope) {
         return x -> ray0 + x * tan(raySlope) - profile.elevationAt(x)
