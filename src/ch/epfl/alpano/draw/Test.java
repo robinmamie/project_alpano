5de6ef3d638cd4314a20800f8d67b384cf96cc66
Robin G.F. Mamie <robin.mamie@gmail.com>
2017-04-24 17:48:58 +0200

    First working (?) version of part 9.

--- src/ch/epfl/alpano/draw/Test.java
+++ src/ch/epfl/alpano/draw/Test.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import ch.epfl.alpano.dem.ContinuousElevationModel;
+import ch.epfl.alpano.dem.SuperHgtDiscreteElevationModel;
+import ch.epfl.alpano.gui.Labelizer;
+import ch.epfl.alpano.gui.PredefinedPanoramas;
 import ch.epfl.alpano.summit.GazetteerParser;
 import ch.epfl.alpano.summit.Summit;
 
@@ -12,8 +16,11 @@ public class Test {
     public static void main(String[] args) throws IOException {
         long start = System.nanoTime();
         List<Summit> l = GazetteerParser.readSummitsFrom(new File("alps.txt"));
+        new Labelizer(
+                new ContinuousElevationModel(
+                        new SuperHgtDiscreteElevationModel()),
+                l).labels(PredefinedPanoramas.NIESEN.panoramaParameters());
         long stop = System.nanoTime();
-        l.forEach(System.out::println);
         System.out.printf("%nThe test file took %.3f ms to run.%n",
                 (stop - start) * 1e-6);
     }
--- src/ch/epfl/alpano/gui/Labelizer.java
+++ src/ch/epfl/alpano/gui/Labelizer.java
@@ -1,30 +1,45 @@
 package ch.epfl.alpano.gui;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.DoubleUnaryOperator;
 
 import ch.epfl.alpano.Math2;
+import ch.epfl.alpano.PanoramaComputer;
 import ch.epfl.alpano.PanoramaParameters;
 import ch.epfl.alpano.dem.ContinuousElevationModel;
 import ch.epfl.alpano.dem.ElevationProfile;
 import ch.epfl.alpano.summit.Summit;
 import javafx.scene.Node;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 
 public final class Labelizer {
 
     private final ContinuousElevationModel cem;
     private final List<Summit> summits;
+    private Map<Summit, Integer[]> values;
 
     public Labelizer(ContinuousElevationModel cem, List<Summit> summits) {
         this.cem = cem;
         this.summits = summits;
+        values = new HashMap<Summit, Integer[]>();
     }
 
     private List<Summit> visibleSummits(PanoramaParameters parameters) {
-        List<Summit> visible = new ArrayList<Summit>();
+        List<Summit> visible = new ArrayList<>();
 
         for (Summit s : summits) {
             // Première condition, se trouve dans la zone visible
+            double distance = parameters.observerPosition()
+                    .distanceTo(s.position());
+            if (distance > parameters.maxDistance())
+                continue;
+
             double azimuth = parameters.observerPosition()
                     .azimuthTo(s.position());
             if (Math.abs(Math2.angularDistance(azimuth,
@@ -32,22 +47,37 @@ public final class Labelizer {
                             .horizontalFieldOfView() / 2.)
                 continue;
 
-            double distance = parameters.observerPosition()
-                    .distanceTo(s.position());
-            if (distance > parameters.maxDistance())
-                continue;
+            ElevationProfile profile = new ElevationProfile(cem,
+                    parameters.observerPosition(), azimuth, distance);
 
-            double altitude = Math.atan2(
-                    s.elevation() - parameters.observerElevation(), distance);
+            DoubleUnaryOperator g = PanoramaComputer.rayToGroundDistance(
+                    profile, parameters.observerElevation(), 0);
+
+            double altitude = Math.atan2(-g.applyAsDouble(distance), distance);
             if (Math.abs(altitude) > parameters.verticalFieldOfView() / 2.)
                 continue;
 
             // Seconde condition, est réellement visible par l'observateur.
-            ElevationProfile profile = new ElevationProfile(cem,
-                    parameters.observerPosition(), azimuth, distance);
-            
-            // firstIntevalContainingRoot
+            double cross = Math2.firstIntervalContainingRoot(
+                    PanoramaComputer.rayToGroundDistance(profile,
+                            parameters.observerElevation(), Math.tan(altitude)),
+                    0, distance - 200, 64);
+            if (cross == Double.POSITIVE_INFINITY) {
+                visible.add(s);
+
+                values.put(s, new Integer[] {
+                        (int) Math.round(parameters.xForAzimuth(azimuth)),
+                        (int) Math.round(parameters.yForAltitude(altitude)) });
+
+            }
         }
+
+        visible.sort((x, y) -> {
+            int higher = Integer.compare(values.get(x)[1], values.get(y)[1]);
+            if (higher == 0)
+                return Integer.compare(y.elevation(), x.elevation());
+            return higher;
+        });
 
         return visible;
     }
@@ -55,8 +85,41 @@ public final class Labelizer {
     public List<Node> labels(PanoramaParameters parameters) {
 
         List<Summit> visible = visibleSummits(parameters);
+        List<Summit> toLabel = new ArrayList<>();
+        List<Node> nodes = new ArrayList<>();
 
-        return null;
+        int labelPlace = -1;
+
+        for (Summit s : visible) {
+            int xIndex = values.get(s)[0];
+            int yIndex = values.get(s)[1];
+            if (yIndex > 170) {
+                if (labelPlace == -1)
+                    labelPlace = yIndex - 22;
+                if (19 <= xIndex && xIndex < parameters.width() - 19) {
+                    boolean toAdd = true;
+                    for (Summit l : toLabel)
+                        if (Math.abs(values.get(l)[0] - xIndex) < 20)
+                            toAdd = false;
+                    if (toAdd) {
+                        toLabel.add(s);
+                        Text t = new Text(
+                                s.name() + " (" + s.elevation() + " m)");
+                        t.getTransforms().addAll(new Translate(xIndex, yIndex),
+                                new Rotate(30, 0, 0));
+                        nodes.add(t);
+                        Line l = new Line();
+                        l.setStartX(xIndex);
+                        l.setStartY(labelPlace);
+                        l.setEndX(xIndex);
+                        l.setEndY(yIndex);
+                        nodes.add(l);
+                    }
+                }
+            }
+        }
+
+        return nodes;
     }
 
 }
